971750275eeccce0f6ad28a237190bc9646be847
root <root@p-6tbptdasrnmr-workstation.c.instruqt-prod.internal>
2021-05-19 14:13:44 +0000

    Update security rules for http/https

--- main.tf
+++ main.tf
@@ -38,14 +38,14 @@ resource "aws_security_group" "hashicat" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["98.118.109.90/32"]
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["98.118.109.90/32"]
   }
 
   egress {
